432e9bc94e28070017189720ebfacba6ad15d13f
Panos Oikonomopoulos <panos2072@gmail.com>
2023-12-29 15:13:18 +0200

    Somewhat finished missing the comfirmed bookings

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-
-// Import your page components
-import HomePage from './components/HomePage'; // Adjust the import path as necessary
-import BookingPage from './components/BookingPage'; // Adjust the import path as necessary
-// Import other components you need for different routes
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          {/* Place your Nav component here if you have one */}
-        </header>
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/booking" element={<BookingPage />} />
-          {/* Add more routes as needed */}
-        </Routes>
-      </div>
-    </Router>
+    <div>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
+    </div>
   );
 }
 
